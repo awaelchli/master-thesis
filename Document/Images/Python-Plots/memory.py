b2609f74d495b17874cc3784a6b1a107a3870d37
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2017-11-07 18:46:47 +0100

    updated plots, plot for multiclass

--- Document/Images/Python-Plots/memory.py
+++ Document/Images/Python-Plots/memory.py
@@ -8,6 +8,7 @@ width = 0.5
 linetype = 'bo-'
 show = False
 
+
 #########################
 # Look-back vs. Accuracy
 #########################
@@ -19,7 +20,7 @@ y = (0.9999, 0.7495, 0.7208, 0.6452, 0.6744)
 #plt.bar(x, y, width, color='b')
 plt.plot(x, y, linetype)
 plt.xticks(x)
-plt.ylim((0.5, 1.01))
+plt.ylim((0.5, 1.05))
 
 # add some text for labels, title and axes ticks
 #plt.title('Accuracy vs. Look-back')
@@ -27,7 +28,7 @@ plt.ylabel('Accuracy')
 plt.xlabel('Look-back m')
 plt.grid()
 
-plt.savefig('./accuracy-vs-look-back.pdf', bbox_inches='tight')
+plt.savefig('./memory/accuracy-vs-look-back.pdf', bbox_inches='tight')
 
 
 if show:
@@ -45,7 +46,7 @@ y = (0.6744, 0.7404, 0.8276, 0.9258, 0.9433, 1.0000, 1.0000)
 #plt.bar(x, y, width, color='b')
 plt.plot(x, y, linetype)
 plt.xticks(x)
-plt.ylim((0.5, 1.01))
+plt.ylim((0.5, 1.05))
 
 # add some text for labels, title and axes ticks
 #plt.title('Accuracy vs. Hidden Size')
@@ -53,7 +54,7 @@ plt.ylabel('Accuracy')
 plt.xlabel('Hidden Size d')
 plt.grid()
 
-plt.savefig('./accuracy-vs-hidden-size.pdf', bbox_inches='tight')
+plt.savefig('./memory/accuracy-vs-hidden-size.pdf', bbox_inches='tight')
 
 if show:
     plt.show()
@@ -70,7 +71,7 @@ y = (1.0000, 1.0000, 1.0000, 0.9892, 0.9720, 0.9125, 0.8691, 0.7456, 0.6877)
 #plt.bar(x, y, width, color='b')
 plt.plot(x, y, linetype)
 plt.xticks(x)
-plt.ylim((0.5, 1.01))
+plt.ylim((0.5, 1.05))
 
 # add some text for labels, title and axes ticks
 #plt.title('Accuracy vs. Look-back')
@@ -78,7 +79,7 @@ plt.ylabel('Accuracy')
 plt.xlabel('Look-back m')
 plt.grid()
 
-plt.savefig('./accuracy-vs-look-back2.pdf', bbox_inches='tight')
+plt.savefig('./memory/accuracy-vs-look-back2.pdf', bbox_inches='tight')
 
 if show:
     plt.show()
@@ -95,7 +96,7 @@ y = (0.6877, 0.8836, 0.8046, 0.9484, 0.9943)
 #plt.bar(x, y, width, color='b')
 plt.plot(x, y, linetype)
 plt.xticks(x)
-plt.ylim((0.5, 1.01))
+plt.ylim((0.5, 1.05))
 
 # add some text for labels, title and axes ticks
 #plt.title('Accuracy vs. Look-back')
@@ -103,7 +104,35 @@ plt.ylabel('Accuracy')
 plt.xlabel('Amount of data N')
 plt.grid()
 
-plt.savefig('./more-training-data.pdf', bbox_inches='tight')
+plt.savefig('./memory/more-training-data.pdf', bbox_inches='tight')
+
+if show:
+    plt.show()
+
+
+##################################
+# Multiple classes and more layers
+##################################
+plt.figure(figsize=(5, 3))
+
+x = tuple(range(2, 16))
+y1 = (1.0000, 0.9999, 0.9998, 0.9987, 0.9957, 0.9704, 0.9343, 0.9129, 0.8365, 0.7561, 0.7114, 0.6656, 0.6522, 0.5748)
+y2 = (1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 0.9999, 0.9998, 0.9998, 0.9992, 0.9801, 0.9883, 0.9730, 0.9313, 0.9317)
+
+#plt.bar(x, y, width, color='b')
+plt.plot(x, y1, 'bo-', label='One layer')
+plt.plot(x, y2, 'ro-', label='Two layers')
+plt.xticks(x)
+plt.ylim((0.5, 1.05))
+plt.legend(loc=3)
+
+# add some text for labels, title and axes ticks
+#plt.title('Accuracy vs. Look-back')
+plt.ylabel('Accuracy')
+plt.xlabel('Number of classes C')
+plt.grid()
+
+plt.savefig('./memory/accuracy-vs-multiple-classes-and-layers.pdf', bbox_inches='tight')
 
 if show:
     plt.show()
